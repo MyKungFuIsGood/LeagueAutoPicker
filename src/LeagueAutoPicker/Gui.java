523f23bc1979e6bb11fb4547e824194e0692a330
Ben Smith <benwsmith@gmail.com>
2014-01-24 16:02:01 -0700

    delete random //'s

--- src/LeagueAutoPicker/Gui.java
+++ src/LeagueAutoPicker/Gui.java
@@ -6,7 +6,7 @@ import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
-import java.io.File; /////
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
