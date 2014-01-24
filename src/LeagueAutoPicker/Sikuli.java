ab9c46d7781b4d185e41364221097e9cbcc7253d
Ben Smith <benwsmith@gmail.com>
2014-01-24 16:08:13 -0700

    Set a minScore match to 0.7 for imgFind
    Means the picture must be very close to similar to be considered a
    match.

--- src/LeagueAutoPicker/Sikuli.java
+++ src/LeagueAutoPicker/Sikuli.java
@@ -32,6 +32,7 @@ public class Sikuli {
 		}
 		
 		Target target = new ImageTarget(image);
+		target.setMinScore(0.8); // 1 is perfect image match, 0 is blurry match
 		ScreenRegion s = new DesktopScreenRegion();
 		ScreenRegion r = s.find(target);
 		
@@ -41,58 +42,5 @@ public class Sikuli {
 		}
 		
 		return location;
-	}
-	
-	public static int[] findHero( String path ) {
-		int[] location = {0,0};
-		
-		BufferedImage image = null;
-		try {
-			image = ImageIO.read(new File(path));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		image = Sikuli.heroImageCrop(image);
-		
-		Target target = new ImageTarget(image);
-		
-		//ScreenRegion s = new DekstopScreenRegion();
-		// smaller ScreenRegion means faster searches.
-		ScreenRegion s = new DesktopScreenRegion(270, 215, 1380, 650);
-		
-		ScreenRegion r = s.find(target);
-		
-		if( r != null )  {
-			location[0] = r.getCenter().getX();
-			location[1] = r.getCenter().getY();
-		}
-		
-		return location;
-	}
-	
-	
-	public static BufferedImage heroImageCrop( BufferedImage image ) {		
-		// want to crop a rectangle to the right
-		// cropping to the right makes sure that the hero free week icon does not disrupt the find process
-		image = image.getSubimage(60, 15, 50, 90); 
-		
-		// resize image for desktop resolution
-		// desktop is 1920 * 1200 for some reason on mac retina's
-		// league's image sizes are twice what they need to be, don't know why
-		int h = image.getHeight() / 2;
-		int w = image.getWidth() / 2;
-		BufferedImage tmp = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-		Graphics2D g = tmp.createGraphics();
-		try {
-			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-			g.drawImage(image, 0, 0, w, h, null);
-		}
-		finally {
-			g.dispose();
-		}
-		image = tmp;
-		
-		return image;
 	}
 }
